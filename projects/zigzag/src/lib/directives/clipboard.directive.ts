59901b41c257409dcac9f2f94bd53a2593a6da5e
Adithya Sreyaj <adi.sreyaj@gmail.com>
2022-01-26 01:51:34 +0530

    feat(clipboard): add copy to clipboard directive

---
+++ projects/zigzag/src/lib/directives/clipboard.directive.ts
@@ -0,0 +1,22 @@
+import { Directive, HostListener, Input, NgModule } from '@angular/core';
+import { Clipboard } from '@angular/cdk/clipboard';
+
+@Directive({
+  selector: '[zzClipboard]',
+})
+export class ClipboardDirective {
+  @Input('zzClipboard') text!: string;
+
+  @HostListener('click')
+  onClick() {
+    this.clipboard.copy(this.text);
+  }
+
+  constructor(private clipboard: Clipboard) {}
+}
+
+@NgModule({
+  declarations: [ClipboardDirective],
+  exports: [ClipboardDirective],
+})
+export class ClipboardDirectiveModule {}
--- projects/zigzag/src/public-api.ts
+++ projects/zigzag/src/public-api.ts
@@ -3,3 +3,4 @@
  */
 
 export * from './lib/components';
+export * from './lib/directives';
